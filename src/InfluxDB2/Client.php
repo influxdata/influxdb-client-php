1726d491037400b8ff09dad574708ea5ef92a969
Robert Hajek <robert.hajek@gmail.com>
2020-03-06 13:24:19 +0100

    prepare for next development iteration

--- src/InfluxDB2/Client.php
+++ src/InfluxDB2/Client.php
@@ -7,7 +7,7 @@ class Client
     /**
      * Client version updated by: 'make release VERSION=1.5.0'
      */
-    const VERSION = '1.0.0';
+    const VERSION = 'dev';
 
     public $options;
     public $closed = false;
