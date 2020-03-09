b4002a773abbd2558f5084c707c52de1173bfcbd
Pavlina Rolincova <rolincova@apitea.com>
2020-03-09 12:06:40 +0100

    test: added new tests for DefaultApi checking throwing exceptions

--- tests/DefaultApiTest.php
+++ tests/DefaultApiTest.php
@@ -3,6 +3,8 @@
 namespace InfluxDB2Test;
 
 use GuzzleHttp\Psr7\Response;
+use InvalidArgumentException;
+use InfluxDB2\ApiException;
 
 require_once('BasicTest.php');
 
@@ -17,5 +19,35 @@ class DefaultApiTest extends BasicTest
 
         $this->assertStringStartsWith('influxdb-client-php/',
             strval($request->getHeader("User-Agent")[0]));
+    }
+
+    public function testContentType()
+    {
+        $this->mockHandler->append(new Response(204));
+        $this->writeApi->write('h2o,location=west value=33i 15');
+
+        $request = $this->mockHandler->getLastRequest();
+
+        $this->assertNotEmpty($request->getHeader("Content-Type"));
+    }
+
+    public function testApiException()
+    {
+        $this->mockHandler->append(new Response(400));
+
+        $this->expectException(ApiException::class);
+
+        $this->writeApi->write('h2o,location=west value=33i 15');
+    }
+
+    public function testInvalidArgument()
+    {
+        $this->mockHandler->append(new Response(204));
+
+        $this->writeApi->options["org"] = '';
+
+        $this->expectException(InvalidArgumentException::class);
+
+        $this->writeApi->write('h2o,location=west value=33i 15');
     }
 }
