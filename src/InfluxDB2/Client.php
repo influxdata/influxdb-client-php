dc19bb9a47c971ee4da62a9335af2371046235be
Jakub Bednar <jakub.bednar@gmail.com>
2020-07-17 09:50:00 +0200

    chore(release): prepare for next development iteration

--- src/InfluxDB2/Client.php
+++ src/InfluxDB2/Client.php
@@ -9,7 +9,7 @@ class Client
     /**
      * Client version updated by: 'make release VERSION=1.5.0'
      */
-    const VERSION = '1.5.0';
+    const VERSION = 'dev';
 
     public $options;
     public $closed = false;
