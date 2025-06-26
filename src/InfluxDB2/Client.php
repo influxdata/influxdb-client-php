59ac11d63ce030973c79d5b05797813761a0d58e
Jakub Bednar <jakub.bednar@gmail.com>
2025-06-26 07:12:59 +0200

    chore(release): release influxdb-client-php-3.8.0

--- src/InfluxDB2/Client.php
+++ src/InfluxDB2/Client.php
@@ -15,7 +15,7 @@ class Client
     /**
      * Client version updated by: 'make release VERSION=1.5.0'
      */
-    const VERSION = 'dev';
+    const VERSION = '3.8.0';
 
     public $options;
     public $closed = false;
